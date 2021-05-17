052d49ee0cd9d5eb91eb62697044799b7dfd62ce
xul8tr <xul8tr@gmail.com>
2021-05-17 04:22:40 -0500

    Update instbin.py

--- instbin.py
+++ instbin.py
@@ -1,5 +1,5 @@
 import os
 os.system("cd c:/windows/system32/")
-os.system("git clone https://github.com/sakurai07/checkra1n.py")
+os.system("git clone https://github.com/xul8tr/checkra1n.py")
 print("installing to bin, you can delete the folder now")
 os.system("copy checkra1n.py/checkra1n.exe c:/windows/system32")
